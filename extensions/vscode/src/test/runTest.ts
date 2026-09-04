8c776b201a7cfffc24059fa3059ab1f54c033d00
Rio Fujita <rio_github@rio.st>
2026-09-04 18:35:50 +0900

    Stabilize VS Code Extension Host CI

--- extensions/vscode/src/test/runTest.ts
+++ extensions/vscode/src/test/runTest.ts
@@ -4,7 +4,11 @@ import { runTests } from "@vscode/test-electron";
 async function main(): Promise<void> {
   const extensionDevelopmentPath = path.resolve(__dirname, "../..");
   const extensionTestsPath = path.resolve(__dirname, "suite", "index");
-  await runTests({ extensionDevelopmentPath, extensionTestsPath });
+  await runTests({
+    version: process.env.VSCODE_TEST_VERSION ?? "1.100.0",
+    extensionDevelopmentPath,
+    extensionTestsPath
+  });
 }
 
 main().catch((error: unknown) => {
